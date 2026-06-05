30d9827e61289584e8860b3ebf38c91156852149
sharomzm <smzm022006@gmail.com>
2026-06-04 20:45:54 -0600

    se agrego el modo oscuro, un boton de menu u ajustarse a diferentes pantallas

--- js/script.js
+++ js/script.js
@@ -22,3 +22,10 @@ boton.addEventListener("click", () => {
     }
 
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
