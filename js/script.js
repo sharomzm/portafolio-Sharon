7d769f139f98cfe2ad0165898e1fc6115af8a1fd
sharomzm <smzm022006@gmail.com>
2026-06-06 00:48:47 -0600

    se agrego agrego la funcion de enviar el correo

--- js/script.js
+++ js/script.js
@@ -29,3 +29,31 @@ const menu = document.getElementById("menu");
 menuToggle.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+// Inicializar EmailJS
+(function () {
+    emailjs.init({
+        publicKey: "AEE-prpuK76Fd-a2D",
+    });
+})();
+
+const formulario = document.getElementById("contact-form");
+
+formulario.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.send("service_8suwozz", "template_b2acaa7", {
+        nombre: document.getElementById("nombre").value,
+        email: document.getElementById("email").value,
+        mensaje: document.getElementById("mensaje").value
+    })
+    .then((response) => {
+        console.log("Éxito:", response);
+        alert("Mensaje enviado correctamente");
+        formulario.reset();
+    })
+    .catch((error) => {
+        console.error("Error EmailJS:", error);
+        alert("Error al enviar el mensaje");
+    });
+});
